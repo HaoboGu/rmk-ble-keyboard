9541cb175e6d27c9bcf9fe7fa583649cb04ccb24
HaoboGu <haobogu@outlook.com>
2024-05-19 14:54:26 +0800

    update firmware project
    Signed-off-by: HaoboGu <haobogu@outlook.com>

--- firmware/rmk-nrf52840/src/keymap.rs
+++ firmware/rmk-nrf52840/src/keymap.rs
@@ -5,15 +5,25 @@ use rmk::{a, k, layer, mo};
 #[rustfmt::skip]
 pub static KEYMAP: [[[KeyAction; COL]; ROW]; NUM_LAYER] = [
     layer!([
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown)],
-        [k!(Kp4), k!(LShift), k!(Kp6)],
-        [mo!(1), k!(Kp2), k!(Kp3)],
-        [mo!(1), a!(No), k!(Kp0)]
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)]
     ]),
     layer!([
-        [k!(Kp7), k!(Kp8), k!(Kp9)],
-        [k!(Kp4), k!(LCtrl), k!(Kp6)],
-        [mo!(1), k!(Kp2), k!(Kp3)],
-        [mo!(1), a!(No), k!(Kp0)]
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)]
+    ]),
+   
+    layer!([
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
+        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)]
     ]),
 ];
