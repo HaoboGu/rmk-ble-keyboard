bac214a5c15f9d8c97b84379a1658396fbcbe999
HaoboGu <haobogu@outlook.com>
2024-05-30 23:25:52 +0800

    feat: update firmware
    Signed-off-by: HaoboGu <haobogu@outlook.com>

--- firmware/rmk-nrf52840/src/keymap.rs
+++ firmware/rmk-nrf52840/src/keymap.rs
@@ -5,25 +5,24 @@ use rmk::{a, k, layer, mo};
 #[rustfmt::skip]
 pub static KEYMAP: [[[KeyAction; COL]; ROW]; NUM_LAYER] = [
     layer!([
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)]
+        [k!(Grave), k!(Kc1), k!(Kc2), k!(Kc3), k!(Kc4), k!(Kc5), k!(Kc6), k!(Kc7), k!(Kc8), k!(Kc9), k!(Kc0), k!(Minus), k!(Equal), k!(Backspace)],
+        [k!(Tab), k!(Q), k!(W), k!(E), k!(R), k!(T), k!(Y), k!(U), k!(I), k!(O), k!(P), k!(LeftBracket), k!(RightBracket), k!(Backslash)],
+        [k!(Escape), k!(A), k!(S), k!(D), k!(F), k!(G), k!(H), k!(J), k!(K), k!(L), k!(Semicolon), k!(Quote), a!(No), k!(Enter)],
+        [k!(LShift), k!(Z), k!(X), k!(C), k!(V), k!(B), k!(N), k!(M), k!(Comma), k!(Dot), k!(Slash), a!(No), a!(No), k!(RShift)],
+        [k!(LCtrl), k!(LGui), k!(LAlt), a!(No), a!(No), k!(Space), a!(No), a!(No), a!(No), mo!(1), k!(RAlt), a!(No), k!(RGui), k!(RCtrl)]
     ]),
     layer!([
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)]
+        [k!(Grave), k!(F1), k!(F2), k!(F3), k!(F4), k!(F5), k!(F6), k!(F7), k!(F8), k!(F9), k!(F10), k!(F11), k!(F12), k!(Delete)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
+        [k!(CapsLock), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), k!(UP)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), k!(Left), a!(No), k!(Down), k!(Right)]
     ]),
-
     layer!([
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)],
-        [k!(AudioVolUp), k!(B), k!(AudioVolDown), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A), k!(A)]
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)],
+        [a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No), a!(No)]
     ]),
 ];
